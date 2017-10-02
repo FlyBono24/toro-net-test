20b1f0b92f80a7846e31966dd82bfc15d9eb73b0
Matthew Levan <matt.s.levan@gmail.com>
2017-10-01 18:15:18 -0700

    Updated dev-env.sh

--- dev-env.sh
+++ dev-env.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y mongodb-org &&
 sudo service mongod start &&
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - &&
 sudo apt-get install -y nodejs &&
-cd /home/ubuntu/toro-net-test/ && npm install
+npm install
